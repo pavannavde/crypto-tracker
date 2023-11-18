d5f33b1fb94c760419ab3a07d1ec15d82b7b414a
Your Name <youremail@example.com>
2023-11-18 11:35:16 +0530

    theme changre

--- src/components/Common/Header/drawer.js
+++ src/components/Common/Header/drawer.js
@@ -1,11 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Drawer from "@mui/material/Drawer";
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
 import { IconButton } from "@mui/material";
 import { Link } from "react-router-dom";
+import {Switch} from "@mui/material";
+import { toast } from "react-toastify";
 
 export default function TemporaryDrawer() {
   const [open, setOpen] = useState(false);
+  const [darkMode, setDarkMode] = useState(
+    localStorage.getItem("theme") == "dark" ? true : false
+  );
+
+  useEffect(() => {
+    if (localStorage.getItem("theme") == "dark") {
+      setDark();
+    } else {
+      setLight();
+    }
+  }, []);
+
+  const changeMode = () => {
+    setDarkMode(!darkMode);
+    toast.success("Theme Changed!");
+    const mode = localStorage.getItem("theme");
+    if (mode == "dark") {
+      setLight();
+    } else {
+      setDark();
+    }
+  };
+
+  const setDark = () => {
+    localStorage.setItem("theme", "dark");
+    document.documentElement.setAttribute("data-theme", "dark");
+  };
+
+  const setLight = () => {
+    localStorage.setItem("theme", "light");
+    document.documentElement.setAttribute("data-theme", "light");
+  };
 
   return (
     <div>
@@ -26,6 +60,13 @@ export default function TemporaryDrawer() {
           <Link to="/Dashboard">
             <p className="link">Dashboard</p>
           </Link>
+          <p className="link">{darkMode ? "Light Mode" : "Dark Mode"}</p>
+            <Switch
+              checked={darkMode}
+              onClick={() => {
+                changeMode();
+              }}
+            />
         </div>
       </Drawer>
     </div>
--- src/components/Common/Header/index.js
+++ src/components/Common/Header/index.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './styles.css'
 import TemporaryDrawer from './drawer';
 import Button from '../Button';
 import { Link } from 'react-router-dom';
+import { Switch }  from '@mui/material';
+import { toast } from 'react-toastify';
 
 const Header = () => {
+  const [darkMode, setDarkMode] = useState(
+    localStorage.getItem("theme") == "dark" ? true : false
+  );
+
+  useEffect(() => {
+    if (localStorage.getItem("theme") == "dark") {
+      setDark();
+    } else {
+      setLight();
+    }
+  }, []);
+
+  const changeMode = () => {
+    setDarkMode(!darkMode);
+    toast.success("Theme Changed!");
+    const mode = localStorage.getItem("theme");
+    if (mode == "dark") {
+      setLight();
+    } else {
+      setDark();
+    }
+  };
+
+  const setDark = () => {
+    localStorage.setItem("theme", "dark");
+    document.documentElement.setAttribute("data-theme", "dark");
+  };
+
+  const setLight = () => {
+    localStorage.setItem("theme", "light");
+    document.documentElement.setAttribute("data-theme", "light");
+  };
+
   return (
     <div className='header'>
 
@@ -13,6 +48,11 @@ const Header = () => {
         </h1>
 
        <div className='links'>
+        <Switch
+        checked={darkMode}
+        onClick={() => {
+          changeMode();}}
+        />
         <Link to='/'>
           <p className='link'>Home</p>
         </Link>
